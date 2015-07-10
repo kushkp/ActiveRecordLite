9720df6ce4fb5a847ee70d63a5ad4b199a6b093d
kushkp <ankushp89@gmail.com>
2015-07-10 16:02:13 -0700

    Finish 03

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -30,8 +30,9 @@ end
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     self.foreign_key = options[:foreign_key] ||
-      "#{self_class_name.downcase}_id".to_sym
-    self.class_name = options[:class_name] || "#{name.singularize.capitalize}"
+      "#{self_class_name}_id".downcase.to_sym
+      # byebug
+    self.class_name = options[:class_name] || "#{name}".singularize.capitalize
     self.primary_key = options[:primary_key] || :id
   end
 end
@@ -45,21 +46,38 @@ module Associatable
       fk = send(options.foreign_key)
       pk = options.primary_key
       cn = options.model_class
+      results = nil
+      results = DBConnection.execute(<<-SQL) unless fk.nil?
+        SELECT
+          *
+        FROM
+          #{options.table_name}
+        WHERE
+         #{pk} = #{fk};
+      SQL
+
+      results.nil? ? nil : cn.new(results.first)
+    end
+  end
+
+  def has_many(name, options = {})
+    options = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      fk = options.foreign_key
+      pk = send(options.primary_key)
+      cn = options.model_class
       results = DBConnection.execute(<<-SQL)
         SELECT
           *
         FROM
           #{options.table_name}
         WHERE
-          #{pk} = #{fk};
+          #{fk} = #{pk};
       SQL
 
-      cn.new(results.first)
+      results.map{ |attributes| cn.new(attributes) }
     end
-  end
-
-  def has_many(name, options = {})
-
   end
 
   def assoc_options
